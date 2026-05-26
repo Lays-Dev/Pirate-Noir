df1cd057a470feb514b98bc66a4ab098cf5bc48a
Lays-Dev <penn2ink@gmail.com>
2026-05-26 19:16:23 -0400

    Working Base, deleted broken stuff

--- Pirate-Noir/Assets/Scripts/Lays Scripts/Interaction System/Core/Interactable.cs
+++
@@ -1,8 +0,0 @@
-public interface Interactable
-{
-    // Defining functions
-     public bool CanInteract();
-     //Takes Interactor as a parameter. Allows the script to access the Interactor script
-    public bool Interact(Interactor interactor);
-
-}
