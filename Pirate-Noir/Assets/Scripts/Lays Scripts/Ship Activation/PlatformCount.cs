27fa95291ac3e6626e2d651b3b5d4ad32890a654
Lays-Dev <penn2ink@gmail.com>
2026-05-27 22:35:52 -0400

    Tilting platforms! WORKING

--- Pirate-Noir/Assets/Scripts/Lays Scripts/Ship Activation/PlatformCount.cs
+++ Pirate-Noir/Assets/Scripts/Lays Scripts/Ship Activation/PlatformCount.cs
@@ -4,6 +4,25 @@ public class PlatformCount : MonoBehaviour
 {
     public PlatformProgression platformManager;
 
+    private Rigidbody rb;
+    private bool activePhysics = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = false;
+        rb.isKinematic = true; // IMPORTANT: fully freeze them
+    }
+
+    public void ActivatePhysics()
+    {
+        activePhysics = true;
+
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -15,7 +34,6 @@ public class PlatformCount : MonoBehaviour
     public void Die()
     {
         platformManager.EnemyDefeated();
-
         Destroy(gameObject);
     }
 }
--- Pirate-Noir/Assets/Scripts/Lays Scripts/Ship Activation/PlatformProgression.cs
+++ Pirate-Noir/Assets/Scripts/Lays Scripts/Ship Activation/PlatformProgression.cs
@@ -5,9 +5,21 @@ public class PlatformProgression : MonoBehaviour
     [SerializeField] private int killsNeeded = 2;
     [SerializeField] private GameObject nextObjectToActivate;
 
+    [SerializeField] private Transform platformToRotate;
+
+    [SerializeField] private float rotationSpeed = 10f;
+
     private int currentKills = 0;
     private bool activated = false;
+    private bool rotatePlatform = false;
 
+    private PlatformCount[] enemies;
+
+private void Start()
+{
+    Debug.Log("Kills start = " + currentKills);
+    enemies = FindObjectsByType<PlatformCount>(FindObjectsSortMode.None);
+}
     public void EnemyDefeated()
     {
         if (activated)
@@ -15,11 +27,45 @@ public class PlatformProgression : MonoBehaviour
 
         currentKills++;
 
-        if (currentKills >= killsNeeded)
-        {
-            activated = true;
+    if (currentKills >= killsNeeded)
+    {
+        activated = true;
+        nextObjectToActivate.SetActive(true);
+        rotatePlatform = true;
 
-            nextObjectToActivate.SetActive(true);
+        foreach (var enemy in enemies)
+        {
+            enemy.ActivatePhysics();
+        }
+    }
+    }
+
+    private void Update()
+    {
+        if (rotatePlatform)
+        {
+            Vector3 currentRotation = platformToRotate.eulerAngles;
+
+            float xRotation = currentRotation.x;
+
+            // Convert Unity angle to negative range
+            if (xRotation > 180f)
+                xRotation -= 360f;
+
+            // Stop at -35
+            if (xRotation > -60f)
+            {
+                platformToRotate.Rotate(-rotationSpeed * Time.deltaTime, 0f, 0f);
+            }
+            else
+            {
+                rotatePlatform = false;
+
+                Vector3 finalRotation = platformToRotate.eulerAngles;
+                finalRotation.x = -35f;
+
+                platformToRotate.eulerAngles = finalRotation;
+            }
         }
     }
 }
