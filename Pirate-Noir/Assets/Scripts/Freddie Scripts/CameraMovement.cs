ff1573789fcfd6aab8980ca5b6575d3a01d0e3a7
tiptonblu3 <tiptonblu3@gmail.com>
2026-06-02 16:26:30 -0400

    Fixed

--- Pirate-Noir/Assets/Scripts/Freddie Scripts/CameraMovement.cs
+++ Pirate-Noir/Assets/Scripts/Freddie Scripts/CameraMovement.cs
@@ -23,11 +23,15 @@ public class CameraMovement : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext Context)
     {
-       /* if (PauseManagement.GameIsPaused) 
+        if (PauseManagement != null)
         {
-            return;
+            if (PauseManagement.GameIsPaused) 
+            {
+                return;
+            }
         }
-        */
+        
+        
 
         Vector2 LookInput = Context.ReadValue<Vector2>();
 
